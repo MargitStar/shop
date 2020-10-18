43d85e3d742399f7dda37c3e68de59a9f0f24632
MargitStar <sovenokbymargo@mail.ru>
2020-10-18 17:28:06 +0300

    Add series section

--- references/models.py
+++ references/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Author(models.Model):
     author = models.CharField(
         "Author",
@@ -8,3 +9,31 @@ class Author(models.Model):
         null=False
     )
 
+    biography = models.TextField(
+        "About author",
+        max_length=500,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.author
+
+
+class Series(models.Model):
+    title = models.CharField(
+        "Title of series",
+        max_length=100,
+        blank=False,
+        null=False
+    )
+
+    description = models.TextField(
+        "About this series",
+        max_length=500,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.title
