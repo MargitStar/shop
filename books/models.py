35eb5da6a52d1992a69d8ec8bcded62a0eb3562c
MargitStar <sovenokbymargo@mail.ru>
2020-10-20 19:03:58 +0300

    Add fields without relationship

--- books/models.py
+++ books/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from references.models import *
+from datetime import date
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(
+        "Title",
+        max_length=100,
+        blank=False,
+        null=False
+    )
+
+    # cover_photo = models.CharField(
+    #     "Cover"
+    # )
+
+    price = models.DecimalField(
+        "Price in BYN",
+        default=0.0,
+        max_digits=10,
+        decimal_places=2,
+        blank=False,
+        null=False
+    )
+
+    publishing_date = models.DateField(
+        "Date of publishing",
+        default=date.today,
+        blank=False,
+        null=False
+    )
+
+    pages_amount = models.IntegerField(
+        "Amount of pages",
+        default=0,
+        blank=False,
+        null=False
+    )
+
+    binding = models.CharField(
+        "Binding",
+        default="",
+        max_length=50,
+        blank=False,
+        null=False
+    )
+
+    weight = models.IntegerField(
+        "Weight (g)",
+        default=0.0,
+        blank=False,
+        null=False
+    )
+
+    # genre = models.ManyToManyRel(Genre)
+
+    def __str__(self):
+        return self.name
