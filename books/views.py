c05e39df14820e5fac2997133fd29f807b644868
MargitStar <sovenokbymargo@mail.ru>
2020-11-02 16:44:33 +0300

    Refactor book view

--- books/views.py
+++ books/views.py
@@ -10,24 +10,8 @@ def book_list_view(request):
 
 def book_view(request, pk_obj):
     book = models.Book.objects.get(pk=pk_obj)
-    form = forms.BookForm(data={'name': book.name,
-                                'author': book.author,
-                                'genre': book.genre,
-                                'series': book.series,
-                                'publishing_house': book.publishing_house,
-                                'price': book.price,
-                                'format': book.format,
-                                'isbn_code': book.isbn_code,
-                                'publishing_date': book.publishing_date,
-                                'pages_amount': book.pages_amount,
-                                'binding': book.binding,
-                                'weight': book.weight,
-                                'books_amount': book.books_amount,
-                                'age_limit': book.age_limit,
-                                'rating': book.rating,
-                                'is_active': book.is_active
-                                })
-    return render(request, template_name='books/book_view.html', context={'form': form})
+    context = {'book': book}
+    return render(request, template_name='books/book_view.html', context=context)
 
 
 def create_book(request):
@@ -40,6 +24,14 @@ def create_book(request):
         form = forms.CreateBook()
 
     return render(request, template_name='books/create_book.html', context={'form': form})
+
+
+def update_book(request, pk_obj):
+    if request.method == "POST":
+        form = forms.UpdateBook(data=request.POST)
+        if form.is_valid():
+            book = models.Book.objects.get(pk=pk_obj)
+            updated_book = forms.UpdateBook()
 
 
 def delete_book(request, pk_obj):
