8478bd1df8f1ac3f9b032f5331f9a758dec2ee03
MargitStar <sovenokbymargo@mail.ru>
2020-11-08 20:03:37 +0300

    Add change password view

--- auth_view.py
+++ auth_view.py
@@ -1,5 +1,20 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class MyLogInView(LoginView):
     template_name = 'registration/log_in.html'
+    success_url = '/'
+
+
+class MyLogOutView(LogoutView):
+    template_name = 'registration/log-out.html'
+
+
+class MyPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+    template_name = 'registration/password_change.html'
+    success_url = '/'
+
+
+# class MyPasswordChangeDoneView(LoginRequiredMixin, PasswordChangeDoneView):
+#     pass
