d046c459ec3cc184159ef2f80be185c6ca6d8ba2
MargitStar <sovenokbymargo@mail.ru>
2020-11-21 12:19:28 +0300

    Add Delete book in cart url

--- cart/urls.py
+++ cart/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'cart'
 urlpatterns = [
     path('add/', views.AddBookToCart.as_view(), name='cart_update'),
+    path('delete/<int:pk>', views.BookInCartDelete.as_view(), name='book_in_cart_delete'),
     path('', views.CartView.as_view(), name='cart_view')
 ]
