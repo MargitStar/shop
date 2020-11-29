6e3bfbf7f092052d452e3e209f4eb19e11d199a0
MargitStar <sovenokbymargo@mail.ru>
2020-11-29 15:43:33 +0300

    Add urls for order and genre s-admin

--- s_admin/urls.py
+++ s_admin/urls.py
@@ -4,5 +4,10 @@ from s_admin import views
 app_name = 's_admin'
 
 urlpatterns = [
-    path('order/list/', views.SAdminOrderList.as_view(), name='s_admin_order_list')
+    path('order/list/', views.SAdminOrderList.as_view(), name='s_admin_order_list'),
+    path('order/update/<int:pk>/', views.SAdminOrderUpdate.as_view(), name='s_admin_order_update'),
+    path('order/delete/<int:pk>/', views.SAdminOrderDelete.as_view(), name='s_admin_order_delete'),
+
+    path('genre/list/', views.SAdminGenreList.as_view(), name='s_admin_genre_list'),
+    path('genre/create/', views.SAdminGenreCreate.as_view(), name='s_admin_genre_create'),
 ]
