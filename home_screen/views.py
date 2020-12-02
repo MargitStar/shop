97ae5ead13da7e37ea9b91dac7ed32c4632ad280
MargitStar <sovenokbymargo@mail.ru>
2020-12-02 15:38:29 +0300

    Top books by rating

--- home_screen/views.py
+++ home_screen/views.py
@@ -13,6 +13,7 @@ class TopBooksListView(TemplateView):
         context = super().get_context_data(**kwargs)
         filtered = models.Book.objects.order_by('-entry_date')
         most_popular = models.Book.objects.order_by('-price')
+        rating = models.Book.objects.order_by('-rating')
         s = requests.get('https://www.nbrb.by/api/exrates/rates?periodicity=0')
         result = s.json()
         rate = {}
@@ -23,5 +24,5 @@ class TopBooksListView(TemplateView):
                 rate['EUR'] = d.get('Cur_OfficialRate') * d.get('Cur_Scale')
             elif d.get('Cur_Abbreviation') == 'RUB':
                 rate['RUB'] = d.get('Cur_OfficialRate') * d.get('Cur_Scale')
-        return {'books': filtered[0:10], 'most_popular': most_popular[::-1][0:10], 'USD': rate.get('USD'),
-                'EUR': rate.get('EUR'), 'RUB': rate.get('RUB'), 'rate': rate}
+        return {'books': filtered[0:10], 'most_popular': most_popular[::-1][0:10], 'rating': rating[0:10],
+                'USD': rate.get('USD'), 'EUR': rate.get('EUR'), 'RUB': rate.get('RUB'), 'rate': rate}
