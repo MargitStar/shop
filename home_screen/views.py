97355c4b8508334544e12caf5c6c7e796c22ef18
MargitStar <sovenokbymargo@mail.ru>
2020-12-02 15:24:34 +0300

    Add currencies

--- home_screen/views.py
+++ home_screen/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from books import models
 from django.views.generic import TemplateView
+import requests
 
 
 class TopBooksListView(TemplateView):
@@ -9,6 +10,18 @@ class TopBooksListView(TemplateView):
     model = models.Book
 
     def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         filtered = models.Book.objects.order_by('-entry_date')
         most_popular = models.Book.objects.order_by('-price')
-        return {'books': filtered[0:10], 'most_popular': most_popular[::-1][0:10]}
+        s = requests.get('https://www.nbrb.by/api/exrates/rates?periodicity=0')
+        result = s.json()
+        rate = {}
+        for d in result:
+            if d.get('Cur_Abbreviation') == 'USD':
+                rate['USD'] = d.get('Cur_OfficialRate') * d.get('Cur_Scale')
+            elif d.get('Cur_Abbreviation') == 'EUR':
+                rate['EUR'] = d.get('Cur_OfficialRate') * d.get('Cur_Scale')
+            elif d.get('Cur_Abbreviation') == 'RUB':
+                rate['RUB'] = d.get('Cur_OfficialRate') * d.get('Cur_Scale')
+        return {'books': filtered[0:10], 'most_popular': most_popular[::-1][0:10], 'USD': rate.get('USD'),
+                'EUR': rate.get('EUR'), 'RUB': rate.get('RUB'), 'rate': rate}
